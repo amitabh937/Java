b69a446c3dbfd883b76dba3d3047387ae2d28a6e
amitabh937 <165904629+amitabh937@users.noreply.github.com>
2026-05-06 13:28:08 +0530

    Create MepCostOptimizerApplication.java

---
+++ MepCostOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.amit.mep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MepCostOptimizerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MepCostOptimizerApplication.class, args);
+        System.out.println("=== MEP-Cost-Optimizer Started by Amit Kumar ===");
+        System.out.println("ML Model V1: Active | 18 Years Experience Loaded");
+    }
+}
